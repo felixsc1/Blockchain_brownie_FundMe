1dc15001462486b10c98047f54788ee140f7d334
Felix Schlegel <schlegel.felix@gmail.com>
2021-11-29 12:55:51 +0100

    added chainlink mocks for local development chain

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,4 +1,4 @@
-from brownie import FundMe, network, config
+from brownie import FundMe, MockV3Aggregator, network, config
 from scripts.helpful_scripts import get_account
 
 
@@ -10,10 +10,17 @@ def deploy_fund_me():
     if network.show_active() != "development":
         price_feed_address = config['networks'][network.show_active(
         )]['eth_usd_pricefeed']
+    else:
+        print(f'The active network is {network.show_active()}')
+        print('Deploying Mocks...')
+        mock_aggregator = MockV3Aggregator.deploy(
+            18, 2000000000000000000000, {'from': account})
+        price_feed_address = mock_aggregator.address
+        print('Mocks Deployed!')
 
     # pass price feed address to our contract.
     fund_me = FundMe.deploy(price_feed_address,
-                            {'from': account}, publish_source=True)
+                            {'from': account}, publish_source=config['networks'][network.show_active()].get("verify"),)
     print(f'Contract deployed to {fund_me.address}')
 
 
