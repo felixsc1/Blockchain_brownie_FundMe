a1a7d01c51c3be3e62ccefdb42465f156d179122
Felix Schlegel <schlegel.felix@gmail.com>
2021-11-29 14:41:00 +0100

    script to call fund() and withdraw() functions of smart contract

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,10 +1,9 @@
 from brownie import network, config, accounts, MockV3Aggregator
-from web3 import Web3
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ['development', 'local-ganache']
 
-DECIMALS = 18
-STARTING_PRICE = 2000
+DECIMALS = 8
+STARTING_PRICE = 200000000000
 
 
 def get_account():
@@ -17,6 +16,6 @@ def get_account():
 def deploy_mocks():
     if len(MockV3Aggregator) <= 0:
         print('Deploying Mocks...')
-        MockV3Aggregator.deploy(DECIMALS, Web3.toWei(
-            STARTING_PRICE, "ether"), {'from': get_account()})
+        MockV3Aggregator.deploy(DECIMALS,
+                                STARTING_PRICE, {'from': get_account()})
         print('Mocks Deployed!')
