7c2871a9302980a882c0cb69d8eee18b392e5907
Felix Schlegel <schlegel.felix@gmail.com>
2021-11-29 15:00:13 +0100

    added testing function

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -19,6 +19,7 @@ def deploy_fund_me():
     fund_me = FundMe.deploy(price_feed_address,
                             {'from': account}, publish_source=config['networks'][network.show_active()].get("verify"),)
     print(f'Contract deployed to {fund_me.address}')
+    return fund_me
 
 
 def main():
