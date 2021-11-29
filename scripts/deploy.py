8afa37931cd80867d3a016c6a46f63e4c3cfa4e9
Felix Schlegel <schlegel.felix@gmail.com>
2021-11-29 14:11:25 +0100

    Modified script to attach to local ganache gui

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,5 @@
 from brownie import FundMe, MockV3Aggregator, network, config
-from scripts.helpful_scripts import get_account, deploy_mocks
+from scripts.helpful_scripts import get_account, deploy_mocks, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 
 
 def deploy_fund_me():
@@ -7,7 +7,7 @@ def deploy_fund_me():
 
     # if we are on a persistent network like rinkeby, use associated oracle address,
     # otherwise deploy mocks.
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config['networks'][network.show_active(
         )]['eth_usd_pricefeed']
     else:
