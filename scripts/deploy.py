2f3a959cbb8e041a8fa900c51738779318e4bdce
Felix Schlegel <schlegel.felix@gmail.com>
2021-11-29 12:13:23 +0100

    Forking price feed address depending on test network, using brownie-config

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,19 @@
-from brownie import FundMe
+from brownie import FundMe, network, config
 from scripts.helpful_scripts import get_account
 
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({'from': account})
+
+    # if we are on a persistent network like rinkeby, use associated oracle address,
+    # otherwise deploy mocks.
+    if network.show_active() != "development":
+        price_feed_address = config['networks'][network.show_active(
+        )]['eth_usd_pricefeed']
+
+    # pass price feed address to our contract.
+    fund_me = FundMe.deploy(price_feed_address,
+                            {'from': account}, publish_source=True)
     print(f'Contract deployed to {fund_me.address}')
 
 
